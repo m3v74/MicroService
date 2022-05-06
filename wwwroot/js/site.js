ab735e0ebd48d48a0218f5944d99641e0e6eccf3
WIN-IMR8UGEV8G7\Mo <mev74rus@gmail.com>
2022-05-06 18:30:25 +0500

    Реализация микросервисов на C#

---
+++ wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    $(document).on("click", "form[data-id]", function (e) {
+        e.preventDefault();
+        var id = $(this).closest("td").find("input").val();
+    })
+
+    /*var settings = {
+        "url": "api/products/PutProduct/id",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({                        //Взять из формы редактирования
+            "Name": "Name2",
+            "Price": 200,
+            "Description": "Description-Description"
+        }),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });*/
+
+    $.ajax(
+        {
+            url: "api/products/GetProduct",
+            type: "get",
+            success: function (r) {
+                $("#out-table").html($("#view-all").render({ Response: r }));
+                console.table(r);
+            }
+        })
+})
